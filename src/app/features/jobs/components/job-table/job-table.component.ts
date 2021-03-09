d30300e8a2fb4a1d42623fb1da81732db570b1ad
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-03-08 23:33:36 -0500

    Create job table component

---
+++ src/app/features/jobs/components/job-table/job-table.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import { Job } from '../../models/job.model';
+import {JobsService} from '../../services/jobs.service';
+
+@Component({
+  selector: 'app-job-table',
+  templateUrl: './job-table.component.html',
+  styleUrls: ['./job-table.component.css']
+})
+export class JobTableComponent implements OnInit {
+
+  jobs: Job[] = [];
+
+  constructor(private jobService: JobsService) {
+
+  }
+
+  ngOnInit(): void {
+    this.jobService.getJobs()
+      .subscribe(jobs => this.jobs = jobs);
+  }
+
+}
