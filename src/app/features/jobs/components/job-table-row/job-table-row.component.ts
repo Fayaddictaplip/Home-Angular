d12c3a3f3482a0d3a8a618fa394657d6bb2154d6
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-03-08 23:33:26 -0500

    Create job table row component

---
+++ src/app/features/jobs/components/job-table-row/job-table-row.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Job} from '../../models/job.model';
+
+@Component({
+  selector: '[app-job-table-row]',
+  templateUrl: './job-table-row.component.html',
+  styleUrls: ['./job-table-row.component.css']
+})
+export class JobTableRowComponent implements OnInit {
+
+  @Input() job: Job;
+
+  ngOnInit(): void {
+  }
+}
